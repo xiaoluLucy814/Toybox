d56e9fd09940973829a1f48bc033dabc47325dbf
Zeyi Liu <lucialiu814@gmail.com>
2020-11-05 17:57:23 +0800

    implement the read-log-and-replay functionality

--- object_playback.py
+++ object_playback.py
@@ -6,6 +6,123 @@ import struct
 import os
 import csv
 
+
+def readLogFile(log_file):
+    log = []
+    with open(log_file, 'r') as f:
+        for row in csv.reader(f):
+            log.append(row)
+    return log
+
+
+def replay_states(log_file, object_id):
+    cid = p.connect(p.SHARED_MEMORY)
+    if cid < 0:
+        p.connect(p.GUI)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+    p.resetSimulation()
+    p.loadURDF("plane.urdf")
+
+    offset_x, offset_y = 0, 0
+    urdf_path, rotation, offset_z = loader.fetch_mobility_object(object_id)
+    object = p.loadURDF(
+        urdf_path,
+        basePosition=[offset_x, offset_y, offset_z],
+        baseOrientation=p.getQuaternionFromEuler(rotation),
+        useFixedBase=True
+    )
+
+    p.setGravity(0, 0, -9.81)
+    p.setRealTimeSimulation(0)
+
+    log = readLogFile(log_file)
+
+    recordNum = len(log)
+    itemNum = len(log[0])
+    print('record num:'),
+    print(recordNum)
+    print('item num:'),
+    print(itemNum)
+
+    for record in log[1:]:
+        jointIndex = int(record[1])
+        position, velocity, force = float(record[2]), float(record[3]), float(record[-1])
+        p.setJointMotorControl2(object,
+                                jointIndex,
+                                p.VELOCITY_CONTROL,
+                                targetPosition=position,
+                                targetVelocity=velocity,
+                                force=force)
+        p.stepSimulation()
+        time.sleep(1. / 20.)
+
+
+def log_states(filename, object_id):
+    cid = p.connect(p.SHARED_MEMORY)
+    if cid < 0:
+        p.connect(p.GUI)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+    p.resetSimulation()
+    p.loadURDF("plane.urdf")
+
+    offset_x, offset_y = 0, 0
+    urdf_path, rotation, offset_z = loader.fetch_mobility_object(object_id)
+    object = p.loadURDF(
+        urdf_path,
+        basePosition=[offset_x, offset_y, offset_z],
+        baseOrientation=p.getQuaternionFromEuler(rotation),
+        useFixedBase=True
+    )
+
+    p.setGravity(0, 0, -9.81)
+    p.setRealTimeSimulation(0)
+
+    numJoints = p.getNumJoints(object)
+    for i in range(numJoints):
+        p.enableJointForceTorqueSensor(object, i, enableSensor=True)
+
+    records = []
+    # log records in 100 frames
+    for timestamp in range(100):
+        p.stepSimulation()
+        for i in range(numJoints):
+            records.append([timestamp, i, p.getJointState(object, i)])
+        time.sleep(1. / 10.)
+
+    dump2file(filename, records)
+    p.disconnect()
+
+
+def dump2file(filename, record):
+    f = open(filename, 'w')
+    f.write('[timestamp], jointIndex, position, velocity, fx, fy, fz, mx, my, mz, appliedJointMotorTorque\n')
+    for timestamp, jointIndex, data in record:
+        f.write("[" + str(timestamp) + "], " + str(jointIndex) + ", ")
+        for i in range(len(data)):
+            if i == 2:
+                for ele in data[i]:
+                    f.write(str(ele) + ", ")
+            elif i == len(data) - 1:
+                f.write(str(data[i]) + "\n")
+            else:
+                f.write(str(data[i]) + ", ")
+
+    f.close()
+
+
+if __name__ == "__main__":
+    object_id = '100968'
+    log_file = os.path.join("log", "{}.TXT".format(object_id))
+    if os.path.exists(log_file):
+        print("LOADING LOG STATES")
+        replay_states(log_file, object_id)
+    else:
+        print("LOGGING STATES")
+        log_states(log_file, object_id)
+
+
 '''
 # function to read and parse a given log file
 def readLogFile(filename, verbose=True):
@@ -80,128 +197,3 @@ def log_states(log_file, object_id):
     p.stopStateLogging(logId)
     p.disconnect()
 '''
-
-
-def readLogFile(log_file):
-    log = []
-    with open(log_file, 'r') as f:
-        for row in csv.reader(f):
-            log.append(row)
-    return log
-
-
-# TODO: replay states recorded in a given log file
-def replay_states(log_file, object_id):
-    cid = p.connect(p.SHARED_MEMORY)
-    if cid < 0:
-        p.connect(p.GUI)
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
-    p.resetSimulation()
-    p.loadURDF("plane.urdf")
-
-    offset_x, offset_y = 0, 0
-    urdf_path, rotation, offset_z = loader.fetch_mobility_object(object_id)
-    object = p.loadURDF(
-        urdf_path,
-        basePosition=[offset_x, offset_y, offset_z],
-        baseOrientation=p.getQuaternionFromEuler(rotation),
-        useFixedBase=True
-    )
-
-    p.setGravity(0, 0, -9.81)
-    p.setRealTimeSimulation(0)
-
-    log = readLogFile(log_file)
-
-    recordNum = len(log)
-    itemNum = len(log[0])
-    print('record num:'),
-    print(recordNum)
-    print('item num:'),
-    print(itemNum)
-
-    for record in log:
-        print(record)
-        # pos = [record[3], record[4], record[5]]
-        # orn = [record[6], record[7], record[8], record[9]]
-        # numJoints = p.getNumJoints(object)
-        # targetPositionsJoints = [record[29]]
-        # for i in range(numJoints):
-        #     jointInfo = p.getJointInfo(object, i)
-        #     qIndex = jointInfo[3]
-        #     if qIndex > -1:
-        #         targetPositionsJoints.append(record[qIndex - 7 + 17])
-        #
-        # p.setJointMotorControlArray(object,
-        #                             range(numJoints),
-        #                             p.POSITION_CONTROL,
-        #                             targetPositions=targetPositionsJoints)
-        #
-        # p.stepSimulation()
-        # time.sleep(1. / 20.)
-
-
-def log_states(filename, object_id):
-    cid = p.connect(p.SHARED_MEMORY)
-    if cid < 0:
-        p.connect(p.GUI)
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
-    p.resetSimulation()
-    p.loadURDF("plane.urdf")
-
-    offset_x, offset_y = 0, 0
-    urdf_path, rotation, offset_z = loader.fetch_mobility_object(object_id)
-    object = p.loadURDF(
-        urdf_path,
-        basePosition=[offset_x, offset_y, offset_z],
-        baseOrientation=p.getQuaternionFromEuler(rotation),
-        useFixedBase=True
-    )
-
-    p.setGravity(0, 0, -9.81)
-    p.setRealTimeSimulation(0)
-
-    numJoints = p.getNumJoints(object)
-    for i in range(numJoints):
-        p.enableJointForceTorqueSensor(object, i, enableSensor=True)
-
-    records = []
-    # log records in 100 frames
-    for timestamp in range(100):
-        p.stepSimulation()
-        for i in range(numJoints):
-            records.append([timestamp, i, p.getJointState(object, i)])
-        time.sleep(1. / 10.)
-
-    dump2file(filename, records)
-    p.disconnect()
-
-
-def dump2file(filename, record):
-    f = open(filename, 'w')
-    f.write('[timestamp], (jointIndex), position, velocity, fx, fy, fz, mx, my, mz, appliedJointMotorTorque\n')
-    for timestamp, jointIndex, data in record:
-        f.write("[" + str(timestamp) + "], (" + str(jointIndex) + "), ")
-        for i in range(len(data)):
-            if i == 2:
-                for ele in data[i]:
-                    f.write(str(ele) + ", ")
-            elif i == len(data) - 1:
-                f.write(str(data[i]) + "\n")
-            else:
-                f.write(str(data[i]) + ", ")
-
-    f.close()
-
-
-if __name__ == "__main__":
-    object_id = '100968'
-    log_file = os.path.join("log", "{}.TXT".format(object_id))
-    if os.path.exists(log_file):
-        print("LOADING LOG STATES")
-        replay_states(log_file, object_id)
-    else:
-        print("LOGGING STATES")
-        log_states(log_file, object_id)
